0daf00454534a680f75ac21cf40d0ce093b25e62
Jens Jákup Egholm Gaardbo <jgaard16@gmail.com>
2018-12-05 21:33:13 +0100

    Added style to pdf gen

--- js/main.js
+++ js/main.js
@@ -15,23 +15,15 @@
   }
 
   function initPDFGenerator(){
-    var doc = new jsPDF();
-    var specialElementHandlers = {
-        '#editor': function (element, renderer) {
-            return true;
-        }
-    };
-
     $('#cmd').click(function () {
       var contentContainer = $("#content");
       contentContainer.append("<h3>"+$("#unitDescription").val()+"</h3>");
       contentContainer.append("<p>"+$("#unitAmount").val()+"</p>");
       contentContainer.append("<p>"+$("#unitPrice").val()+"</p>");
-        doc.fromHTML(contentContainer.html(), 15, 15, {
-            'width': 170,
-            'elementHandlers': specialElementHandlers
-        });
-        doc.save('sample-file.pdf');
+			var pdf = new jsPDF("p", "pt", "a4");
+			pdf.addHTML(contentContainer, function () {
+		     pdf.save('sample-file.pdf');
+		 	});
     });
   }
 
