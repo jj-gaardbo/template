38ef7e1aafd5beac7531229b737bca1d6645c191
Jens Jákup Egholm Gaardbo <jgaard16@gmail.com>
2018-12-05 20:47:32 +0100

    Added pdf export functionality

--- js/main.js
+++ js/main.js
@@ -4,12 +4,35 @@
 
     initRepeater();
 
+    initPDFGenerator();
+
 	});
 
   function initRepeater(){
       $("#repeater").createRepeater({
-          showFirstItemToDefault: false,
+          showFirstItemToDefault: true,
       });
+  }
+
+  function initPDFGenerator(){
+    var doc = new jsPDF();
+    var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+            return true;
+        }
+    };
+
+    $('#cmd').click(function () {
+      var contentContainer = $("#content");
+      contentContainer.append("<h3>"+$("#unitDescription").val()+"</h3>");
+      contentContainer.append("<p>"+$("#unitAmount").val()+"</p>");
+      contentContainer.append("<p>"+$("#unitPrice").val()+"</p>");
+        doc.fromHTML(contentContainer.html(), 15, 15, {
+            'width': 170,
+            'elementHandlers': specialElementHandlers
+        });
+        doc.save('sample-file.pdf');
+    });
   }
 
 })(jQuery);
